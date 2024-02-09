1421c12a5803098441bc7bcc221e09e34ae99ead
Alexandre <xan.96@hotmail.com>
2024-02-08 22:13:24 -0300

    header components

---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import { Feather } from "@expo/vector-icons"
+import { View,Image,Text, TouchableOpacity } from "react-native"
+import colors from "tailwindcss/colors"
+
+type HeaderProps ={
+  title: string
+  cardQuantityItems?: number
+}
+
+export function Header({title, cardQuantityItems= 0}:HeaderProps){
+  return(
+    <View className="flex-row items-center border-b
+    border-slate-700 pb-5 mx-5">
+      <View className="flex-1">
+        <Image source={require("@/assets/logo.png")} className="h-6 w-32 mt-2"/>
+        <Text className="text-white text-xl font-heading mt-2">{title}</Text>
+      </View>
+      {
+        cardQuantityItems>0 &&
+        <TouchableOpacity className="relative" activeOpacity={0.5}>
+        <View className="bg-lime-300 w-4 h-4 rounded-full justify-center 
+        items-center top-2 z-10 -right-3.5">
+          <Text className="text-slate-900 font-bold text-xs">{cardQuantityItems}</Text>
+        </View>
+      
+        <Feather name="shopping-bag" color={colors.white} size={24}/>
+      </TouchableOpacity>}
+    </View>
+
+  )
+}
