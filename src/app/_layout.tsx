85c2a61df5f9d5d37a72d3824d2ee649fd21f049
Alexandre <xan.96@hotmail.com>
2024-02-08 22:12:33 -0300

    building layout main

---
+++ src/app/_layout.tsx
@@ -0,0 +1,32 @@
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts
+} from "@expo-google-fonts/inter"
+
+import { Loading} from "@/components/loading"
+import { Slot} from "expo-router"
+import { SafeAreaView } from "react-native"
+
+
+
+export default function Layout(){
+  const [fontsLoaded]=useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  })
+
+  if(!fontsLoaded){
+    return <Loading/>
+  }
+
+  return(
+    <SafeAreaView className="flex-1 bg-slate-900">
+      <Slot/>
+    </SafeAreaView>
+  )
+}
