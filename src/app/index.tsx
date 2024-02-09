155043b945833e72c46e65ef7d57a6ba9c4c43cb
Alexandre <xan.96@hotmail.com>
2024-02-08 22:12:43 -0300

    building index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,33 @@
-import {View,Text} from "react-native"
+import { useState } from "react"
+import {View,FlatList} from "react-native"
+import { CATEGORIES } from "@/utils/data/products"
+import { CategoryButton } from "@/components/category-button"
+import { Header } from "@/components/header"
 
 export default function Home(){
+  
+ const [category, setCategory]= useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory:string) { 
+    setCategory(selectedCategory)
+  }
+
   return(
-    <View>
-      <Text>React Native!</Text>
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cardQuantityItems={5}/>
+
+      <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item)=>item}
+        renderItem={({item})=>( 
+        <CategoryButton title={item} isSelected={item== category} onPress={()=>handleCategorySelect(item)}/>
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{gap:12, paddingHorizontal:20}}
+      />
+      
     </View>
   )
 }
