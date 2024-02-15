3cd9db983a2f54f26c8c15a2b89a0a8d96c66e84
Alexandre <xan.96@hotmail.com>
2024-02-14 23:30:11 -0300

    config dinamic variable

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,15 +2,19 @@ import {Link} from "expo-router"
 import { useState,useRef } from "react"
 import { Header } from "@/components/header"
 import { Product } from "@/components/products"
-import { CATEGORIES,MENU } from "@/utils/data/products"
+import { CATEGORIES,MENU, ProductProps } from "@/utils/data/products"
 import {View,FlatList,SectionList,Text} from "react-native"
 import { CategoryButton } from "@/components/category-button"
+import { useCartStore } from "@/stores/cart-store"
 
 export default function Home(){
-  
- const [category, setCategory]= useState(CATEGORIES[0])
+  const cartStore = useCartStore();
+    
+  const [category, setCategory]= useState(CATEGORIES[0])
 
- const sectionListRef = useRef<SectionList>(null)
+  const sectionListRef = useRef<SectionList<ProductProps>>(null)
+
+  const cartQualityItems=cartStore.products.reduce((total,product)=>total+product.quantity,0)
 
   function handleCategorySelect(selectedCategory:string) { 
     setCategory(selectedCategory)
@@ -29,7 +33,7 @@ export default function Home(){
 
   return(
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cardQuantityItems={5}/>
+      <Header title="Faça seu pedido" cardQuantityItems={cartQualityItems}/>
 
       <FlatList 
         data={CATEGORIES}
